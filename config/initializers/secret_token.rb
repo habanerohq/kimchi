cca78219fe688ca611a2ee5b97489850ff567b2d
Ben Caldwell <bencaldwell@einti.com>
2013-02-12 12:21:48 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kimchi::Application.config.secret_token = '30d0577a369618e272cc164ccba1b37e531753ab04664b5deecac713616694424ed32915926118a920d268d0c858a0c422219167431ecf737e28bb0f29950e6a'
