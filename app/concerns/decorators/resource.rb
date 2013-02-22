add66a9f245649baaf0b9e6f06c07e59f9c845aa
Ben Caldwell <bencaldwell@einti.com>
2013-02-22 11:28:07 +0800

    Fix a bug with resource decorator concern

--- app/concerns/decorators/resource.rb
+++ app/concerns/decorators/resource.rb
@@ -1,7 +1,10 @@
 module Decorators
   module Resource
     extend ActiveSupport::Concern
-    alias_method :resource, :source
+
+    included do
+      alias_method :resource, :source
+    end
 
     def resource_class
       self.class.source_class
