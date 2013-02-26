f5dbd75b57f07144d20ea31e5c55b3f61ea46663
Ben Caldwell <bencaldwell@einti.com>
2013-02-26 10:16:39 +0800

    More flexible Paginated concern by adding hotspots

--- app/concerns/controllers/paginated.rb
+++ app/concerns/controllers/paginated.rb
@@ -2,8 +2,22 @@ module Controllers
   module Paginated
     extend ActiveSupport::Concern
 
-    included do
-      has_scope :page, :default => 1
+    protected
+
+    def page_param_name
+      :page
+    end
+
+    def current_page
+      params[page_param_name]
+    end
+
+    def per_page
+      resource_class.default_per_page
+    end
+
+    def collection
+      memoize_collection { super.page(current_page).per(per_page) }
     end
   end
 end
