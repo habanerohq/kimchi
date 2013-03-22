112f1446aa86c0e01500f2c34f5d810b0b51226e
Ben Caldwell <bencaldwell@einti.com>
2013-03-22 17:20:03 +0800

    Use singluar table name for factories

--- lib/templates/rspec/model/model_spec.rb
+++ lib/templates/rspec/model/model_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 <% module_namespacing do -%>
 describe <%= class_name %> do
-  subject { FactoryGirl.build(:<%= table_name %>) }
+  subject { FactoryGirl.build(:<%= singular_table_name %>) }
   pending "add some examples to (or delete) #{__FILE__}"
 end
 <% end -%>
